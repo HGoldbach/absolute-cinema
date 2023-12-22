c4790b9780e559ecbbfa006b821b373b15e67989
Goldbach <goldbachcs@gmail.com>
2023-12-21 23:51:39 -0300

    add AppViewModelProvider

---
+++ app/src/main/java/com/goldbach/absolutecinema/ui/AppViewModelProvider.kt
@@ -0,0 +1,21 @@
+package com.goldbach.absolutecinema.ui
+
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.goldbach.absolutecinema.MovieApplication
+import com.goldbach.absolutecinema.ui.viewmodels.HomeViewModel
+
+object AppViewModelProvider {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(
+                movieApplication().container.movieRepository
+            )
+        }
+    }
+}
+
+fun CreationExtras.movieApplication(): MovieApplication =
+    (this[APPLICATION_KEY] as MovieApplication)
